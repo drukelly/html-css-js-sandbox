8f45818b95eee0c430d874f96a6cee2b927a4f02
Dru Kelly <drukelly@gmail.com>
2023-06-17 08:53:00 -0700

    calculator improvements

--- js/calculator.js
+++ js/calculator.js
@@ -1,9 +1,30 @@
-window.addEventListener('click', (e) => {
-  if (e.target.tagName === 'BUTTON') {
-    e.preventDefault();
-    let trace = parseInt(e.target.textContent);
-    let display = document.querySelector('.display');
-    display.textContent = display.textContent === '0' ? trace : display.textContent += trace;
-    console.log(trace);
+const calculator = document.querySelector(".calculator");
+const keys = calculator.querySelector(".numpad");
+const display = document.querySelector(".display");
+
+// add event listener to keys
+keys.addEventListener('click', function (e) {
+  e.preventDefault();
+  const key = e.target;
+  // console.log(key.textContent);
+  // regex to check if key is a number
+  const isNumber = /[0-9]/.test(key.textContent);
+  // regex to check if key is an operator
+  const isOperator = /[x+÷-]/.test(key.textContent);
+  // regex to check if key is equals
+  const isEquals = /[=]/.test(key.textContent);
+  // clear the display back to 0
+  if (key.textContent === 'AC') {
+    display.textContent = '0';
+  }
+  // display can only go up to 9 digits
+  if (isNumber) {
+    display.textContent = display.textContent === '0' ? key.textContent : display.textContent + key.textContent;
+    if (display.textContent.length > 8) {
+      display.textContent = '😪'.repeat(8);
+    }
+  }
+  if (isOperator) {
+    console.log('operator clicked!');
   }
 });
