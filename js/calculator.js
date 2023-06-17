e9a9f5151c4ed22457be02a30b977248946f9702
Dru Kelly <drukelly@gmail.com>
2023-06-16 21:53:31 -0700

    calculator

--- js/calculator.js
+++ js/calculator.js
@@ -2,6 +2,8 @@ window.addEventListener('click', (e) => {
   if (e.target.tagName === 'BUTTON') {
     e.preventDefault();
     let trace = parseInt(e.target.textContent);
+    let display = document.querySelector('.display');
+    display.textContent = display.textContent === '0' ? trace : display.textContent += trace;
     console.log(trace);
   }
 });
